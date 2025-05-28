b366375fd3f73fe03c21c531314198a9889799e2
b3arora <june_1771@icloud.com>
2025-05-28 19:20:17 +0100

    made rotation smoother, changed click to key down

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const atom3 = document.querySelector("#atom-3")
 const cylinder2 = document.querySelector("#cylinder-2")
+const checking = document.querySelector("#marking")
+const moleculeRotation = document.querySelector("#bondGroup")
 let atomHeight = 0.5
 let angleIncrease = -45
 let bondHeight = 0.7
 let clickNo = 0
-const levels = []
-const atoms = []
-const angles = [90, 104.5, 107, 109.5, 120, 180]
-let level = "water"
+let level = 0
+const atom = [104.5, 180]
+const angles = [180, 120, 109.5, 107, 104.5, 90]
+    
 
-atom3.addEventListener("click", () => {
-    if (clickNo < 7) {
-        clickNo++;
-        
-        if (clickNo > 6) {
-            cylinder2.setAttribute("position", `-0.3 ${bondHeight + (0.1*clickNo)} -3`)
-            atom3.setAttribute("position", `-0.62 ${atomHeight + (0.2*clickNo)} -3`)
-            cylinder2.setAttribute("rotation", `0 0 ${angleIncrease + (-15*clickNo)}`)
-        } else if (clickNo <= 6) {
-            cylinder2.setAttribute("position", `-0.4 ${bondHeight + (0.08*clickNo)} -3`)
-            atom3.setAttribute("position", `-0.7 ${atomHeight + (0.15*clickNo)} -3`)
-            cylinder2.setAttribute("rotation", `0 0 ${angleIncrease + (-15*clickNo)}`)
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        if (angles[clickNo] === atom[level]) {
+            checking.setAttribute("value", "correct!")
+            level++
+        } else {
+            checking.setAttribute("value", "wrong! try again or press ? for some help")
+            setTimeout(() => {
+                checking.setAttribute("value", "press enter to submit bond angle")
+            },3000)
+        }
+    } else if (event.key === "i") {
+        if (clickNo < 7) {
+            clickNo++;
+            newBondAngle(clickNo);
+        }
+    } else if (event.key === "d") {
+        if (clickNo > 0) {
+            clickNo--;
+            newBondAngle(clickNo);
         }
     }
-}); 
+})
+
+function newBondAngle(clickNo) {
+    if (clickNo < 7) {
+        moleculeRotation.setAttribute("rotation", `0 0 ${-15*clickNo}`)
+    }
+}
+
